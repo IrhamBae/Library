03ab0b363af18c2bfd750687d65d25d24331e90a
new name <new email>
2024-05-06 07:20:09 +0700

    Library

---
+++ tpmodul11_1302220063/AljabarLibraries/Aljabar.cs
@@ -0,0 +1,33 @@
+﻿namespace AljabarLibraries
+{
+    public class Aljabar
+    {
+        public static double[] AkarPersamaanKuadrat(double[] persamaan)
+        {
+            double a = persamaan[0];
+            double b = persamaan[1];
+            double c = persamaan[2];
+
+            double D = b * b - 4 * a * c; // Diskriminan
+
+            // Menghitung akar-akar
+            double akar1 = (-b + Math.Sqrt(D)) / (2 * a);
+            double akar2 = (-b - Math.Sqrt(D)) / (2 * a);
+
+            return new double[] { akar1, akar2 };
+        }
+
+        public static double[] HasilKuadrat(double[] persamaan)
+        {
+            double a = persamaan[0];
+            double b = persamaan[1];
+
+            // Menghitung hasil kuadrat
+            double a2 = a * a;
+            double b2 = b * b;
+            double ab = 2 * a * b;
+
+            return new double[] { a2, ab, b2 };
+        }
+    }
+}
---
+++ tpmodul11_1302220063/ConsoleApp_Irham/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using AljabarLibraries;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Ambil input persamaan kuadrat dari pengguna
+        Console.WriteLine("\nMasukkan koefisien persamaan kuadrat (a, b, dan c) dipisahkan dengan spasi:");
+        string inputKuadrat = Console.ReadLine();
+        string[] koefisienKuadrat = inputKuadrat.Split(' ');
+
+        // Parsing input ke dalam bentuk double[]
+        double aKuadrat = Convert.ToDouble(koefisienKuadrat[0]);
+        double bKuadrat = Convert.ToDouble(koefisienKuadrat[1]);
+        double cKuadrat = Convert.ToDouble(koefisienKuadrat[2]);
+        double[] persamaanKuadrat = { aKuadrat, bKuadrat, cKuadrat };
+
+        // Panggil fungsi AkarPersamaanKuadrat dari AljabarLibraries
+        double[] akar = Aljabar.AkarPersamaanKuadrat(persamaanKuadrat);
+        Console.WriteLine($"Akar-akar dari persamaan {aKuadrat}x^2 + {bKuadrat}x + {cKuadrat}:");
+        Console.WriteLine($"{akar[0]}, {akar[1]}");
+
+        // Ambil input persamaan linear dari pengguna
+        Console.WriteLine("Masukkan koefisien persamaan linear (a dan b) dipisahkan dengan spasi:");
+        string inputLinear = Console.ReadLine();
+        string[] koefisienLinear = inputLinear.Split(' ');
+
+        // Parsing input ke dalam bentuk double[]
+        double aLinear = Convert.ToDouble(koefisienLinear[0]);
+        double bLinear = Convert.ToDouble(koefisienLinear[1]);
+        double[] persamaanLinear = { aLinear, bLinear };
+
+        // Panggil fungsi HasilKuadrat dari AljabarLibraries
+        double[] hasilKuadrat = Aljabar.HasilKuadrat(persamaanLinear);
+        Console.WriteLine($"Hasil kuadrat dari persamaan {aLinear}x + {bLinear}:");
+        Console.WriteLine($"{hasilKuadrat[0]},{hasilKuadrat[1]},{hasilKuadrat[2]}");
+    }
+}
